42ae760e7fcf12821894439463b0f59b5f20f3af
AmyPerry87 <Amyperry75@hotmail.com>
2024-12-17 11:34:30 +0000

    Add files via upload

---
+++ Task 4 - JavaScript Programming, Variables and Data Types/fortuneTeller.js
@@ -0,0 +1,5 @@
+let myName = prompt("What is your mother's first name?");
+let streetName = prompt("What is the name of the street you grew up on?"); 
+let favouriteColour = prompt("What was your favourite colour as a child?");
+let num1 = prompt("What is your current age?"); 
+let num2 = prompt("Tell me a number between 1 and 10?"); // Output: "In [num2] years you are going to meet your best friend named [myName + streetName]. You will get married in ("num1+num2="+(num1+num2)) years and have [num1 % num2] children.  In Math.round(num1 % num2) years you are going to dye your hair [favouriteColour]."
